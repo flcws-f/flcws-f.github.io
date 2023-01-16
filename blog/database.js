f85bb7cbe327e4968abd067777f42d1104d8abc5
TheHealthOfYou <103227656+TheHealthOfYou@users.noreply.github.com>
2023-01-16 08:58:34 +0800

    Update database.js

--- blog/database.js
+++ blog/database.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+window.onload=function(){
                 //获取文章数据
 $.get("articles"+document.location.hash+".txt",function(data,status){
 $("body").html(data);
 alert(data)
             });
-        });
+        }
