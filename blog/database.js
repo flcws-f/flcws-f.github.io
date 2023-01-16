3710c792eb8024a1bfd7f21bcb1f4970529747f6
TheHealthOfYou <103227656+TheHealthOfYou@users.noreply.github.com>
2023-01-16 09:03:14 +0800

    Delete database.js

--- blog/database.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-                //获取文章数据
-$.get("articles"+document.location.hash+".txt",function(data,status){
-$("body").html(data);
-alert(data)
-            });
-        }
